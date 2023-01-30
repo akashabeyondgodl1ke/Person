e979c19d441cb6286dfb794cdbdda40347d3053e
akashabeyondgodl1ke <123964893+akashabeyondgodl1ke@users.noreply.github.com>
2023-01-30 21:29:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        Person Anatoly = new Person("Анатолий", 32);
+        Person Karl = new Person();
+
+        Anatoly.talk();
+        Karl.move();
+    }
+}
+
+
+public class Person
+{
+    public string FullName;
+    int Age;
+
+    public void talk()
+    {
+        Console.WriteLine($"В данный момент говорит: {FullName}");
+    }
+
+    public void move()
+    {
+        Console.WriteLine($"В данный момент говорит: {FullName}");
+    }
+
+    public Person()
+    {
+
+    }
+
+    public Person(string name, int age)
+    {
+        FullName = name;
+        Age = age;
+    }
+
+}
